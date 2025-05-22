4ed5466b9a36293f3abde315aef7cbbd2ad9dc93
nayna-G <nayanagovind9758@gmail.com>
2025-05-23 00:11:00 +0530

    more edits done to the files and structure changed slightly

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 const snowman = document.querySelector('.snowman');
 
-// Create a flag to know if it should be up or down
 let bounceUp = true;
 
-// Run this code every 500 milliseconds (half a second)
 setInterval(() => {
-  if (bounceUp) {
-    snowman.style.transform = 'translateY(-10px)';
-  } else {
-    snowman.style.transform = 'translateY(0)';
-  }
-  bounceUp = !bounceUp; // Flip the bounce direction
+  snowman.style.transform = bounceUp ? 'translateY(-10px)' : 'translateY(0)';
+  bounceUp = !bounceUp;
 }, 500);
